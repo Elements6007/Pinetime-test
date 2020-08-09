5e7ef3eba255775bb0a292034493a0bd2d525bb1
Sravan <65145081+ZephyrLabs@users.noreply.github.com>
2020-08-09 08:29:00 +0530

    Update Clock.cpp

--- src/DisplayApp/Screens/Clock.cpp
+++ src/DisplayApp/Screens/Clock.cpp
@@ -27,11 +27,7 @@ Clock::Clock(DisplayApp* app,
   displayedChar[2] = 0;
   displayedChar[3] = 0;
   displayedChar[4] = 0;
-
-  //bitmap array
-                                           
-  //bitmap array                                           
-                                             
+                                        
   batteryIcon = lv_label_create(lv_scr_act(), NULL);
   lv_label_set_text(batteryIcon, Symbols::batteryFull);
   lv_obj_align(batteryIcon, lv_scr_act(), LV_ALIGN_IN_TOP_RIGHT, -5, 2);
