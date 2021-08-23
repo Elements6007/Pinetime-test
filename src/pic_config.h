373f1913794271638d03c8955938db62f8c35544
Elements6007v2 <88860704+Elements6007v2@users.noreply.github.com>
2021-08-23 10:32:06 -0400

    Update pic_config.h

--- src/pic_config.h
+++ src/pic_config.h
@@ -1,6 +1,4 @@
 #pragma once
 
 
-#define WF_BACKGROUND_C1        "F:/bg_clock_02.bin"
-#define WF_BACKGROUND_C2        "F:/bg_clock_03.bin"
-#define WF_BACKGROUND_C3        "F:/bg_clock_04.bin
+#define WF_BACKGROUND_C1        "F:/bg_clock_04.bin"
